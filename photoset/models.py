9dd937a8680e3769a84a885f3daf77eab3d76310
muchacos <muchacos@ya.ru>
2019-03-09 15:18:29 +0300

    Photo.Model date, url fields

--- photoset/models.py
+++ photoset/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Photo(models.Model):
     id = models.AutoField(primary_key=True)
-    description = models.TextField(max_length=250)
+    url = models.CharField(max_length=150)
+    description = models.TextField(max_length=400)
+    created_date = models.DateTimeField(default=timezone.now)
