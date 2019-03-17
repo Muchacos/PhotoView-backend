811c068cff96246330c6f98e514f7e0962c70ef1
muchacos <muchacos@ya.ru>
2019-03-17 17:36:57 +0300

    Support Universally Unique ID

--- photoset/models.py
+++ photoset/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.utils import timezone
 from django.core.validators import URLValidator
+import uuid
 
 # Create your models here.
 class Photo(models.Model):
-    id = models.AutoField(primary_key=True)
-    url = models.CharField(max_length=150, null=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     url = models.CharField(max_length=150, null=True, validators=[URLValidator()])
     description = models.TextField(max_length=400, null=True)
     created_date = models.DateTimeField(default=timezone.now)
