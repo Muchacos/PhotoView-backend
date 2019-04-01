d899c5add93bd609d325f69303b39f8b85a3605d
Eugene <shakuskys@gmail.com>
2019-04-01 23:59:53 +0300

    user auth error handling

--- accounts/views.py
+++ accounts/views.py
@@ -7,8 +7,11 @@ from accounts.serializers import UserSerializer
 class UserCreate(APIView):
 
     def post(self, request, format='json'):
+        print('-------', request.data)
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
             if user:
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
